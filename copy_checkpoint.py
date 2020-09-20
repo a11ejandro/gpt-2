1a01bec4c6ddfb21e986b15c02becd91af77343e
Alexander Potrakhov <bendthe@gmail.com>
2020-09-20 22:53:26 +0300

    Mount google drive

--- copy_checkpoint.py
+++ copy_checkpoint.py
@@ -4,9 +4,19 @@ import requests
 import shutil
 from tqdm import tqdm
 
+try:
+    from google.colab import drive
+except:
+    pass
+
 if len(sys.argv) != 2:
     print('You must enter the full checkpoint path as a parameter, e.g. /content/drive/My Drive/gpt_2_checkpoint')
     sys.exit(1)
+
+"""Mounts the user's Google Drive in Colaboratory."""
+assert 'google.colab' in sys.modules, "You must be in Colaboratory to mount your Google Drive"
+
+drive.mount('/content/drive')
 
 file_path = sys.argv[1]
 
